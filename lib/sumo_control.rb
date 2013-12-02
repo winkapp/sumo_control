543fe853528b139624df281f6b866762f5ab1eb7
Allen Madsen <blatyo@gmail.com>
2013-12-02 08:41:21 -0500

    Change where id storage happens

--- lib/sumo_control.rb
+++ lib/sumo_control.rb
@@ -15,21 +15,22 @@ class SumoControl
   def register(collector_id, id_file_path)
     yield (source_definition = SourceDefinition.new)
 
-    register_source(collector_id, source_definition, id_file_path)
+    updated_source_definition = register_source(collector_id, source_definition)
+    store_source_id(updated_source_definition, id_file_path)
+
+    updated_source_definition
   end
 
 private
 
   attr_reader :client
 
-  def register_source(collector_id, source_definition, id_file_path)
-    definition = client.create_source(collector_id, source_definition)
+  def register_source(collector_id, source_definition)
+    client.create_source(collector_id, source_definition)
   rescue SumoControl::Error => error
     raise unless error.duplicate?
 
-    definition = update_server_source(collector_id, source_definition)
-  ensure
-    store_source_id(definition, id_file_path)
+    update_server_source(collector_id, source_definition)
   end
 
   def update_server_source(collector_id, source_definition)
